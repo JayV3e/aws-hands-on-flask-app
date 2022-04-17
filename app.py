da830f66360a607c7cf02cf8468ef259c1e319d0
Jerome Verdoni <jerome.verdoni@ubisoft.com>
2022-04-17 08:13:09 -0400

    Add missing request import

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import date
 
 import boto3
-
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['wsgi']
-    }
-})
 
 app = Flask(__name__)
 
