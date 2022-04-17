cecb2639757b1c25b381fbda7ff00b8176921ac7
Jerome Verdoni <jerome.verdoni@ubisoft.com>
2022-04-17 07:46:57 -0400

    Add boto. Add get/post thankful

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from datetime import date
+
+import boto3
+
 app = Flask(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Thankful')
 
 @app.route("/")
 def hello():
@@ -8,6 +18,28 @@ def hello():
 @app.route("/health")
 def health():
     return jsonify({'status': 'ok'})
+
+@app.route('/thankful', methods=['GET', 'POST'])
+def all_thankfuls():
+    response_object = {'status': 'success'}
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+        
+    if request.method == 'POST':
+        post_data = request.get_json()
+        table.put_item(Item={
+            'Author': post_data.get('author'),
+            'Thankful': post_data.get('thankful'),
+            'Date': date.today().strftime("%d/%m/%Y"),
+        })
+        response_object['message'] = 'Thankful added!'
+    else:
+        response_object['thankful'] = data
+    return jsonify(response_object)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
